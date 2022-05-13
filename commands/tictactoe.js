64bce5ab18d5ee1aa956488d9a18c99e346a2346
aba75671 <assyrianknight26@yahoo.com>
2022-05-13 08:42:44 -0700

    tictactoe functional

--- commands/tictactoe.js
+++ commands/tictactoe.js
@@ -1,20 +1,25 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageActionRow, MessageButton } = require("discord.js");
+const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
+
+let EMPTY = Symbol("empty");
+let PLAYER1 = Symbol("player1");
+let PLAYER2 = Symbol("player2");
+let gameBoardState;
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("tictactoe")
     .setDescription("Starts a game of Tic-Tac-Toe"),
   async execute(interaction) {
-    let EMPTY = Symbol("empty");
-    let initialBoard = [
+    gameBoardState = [
       [EMPTY, EMPTY, EMPTY],
       [EMPTY, EMPTY, EMPTY],
       [EMPTY, EMPTY, EMPTY],
     ];
+    constructGrid();
     await interaction.reply({
-      content: "Tic-Tac-Toe game in progress!",
-      components: constructGrid(),
+      embeds: [new MessageEmbed().setTitle("Turn: X")],
+      components: [components[0], components[1], components[2]],
     });
   },
 };
@@ -27,10 +32,20 @@ function constructGrid() {
 
     for (let column = 0; column < 3; column++) {
       messageButton = new MessageButton()
-        .setCustomId("tictactoe")
-        .setLabel("test")
+        .setCustomId("tictactoe_" + row + "_" + column)
+        .setLabel(" ")
         .setStyle("SECONDARY");
-
+      switch (gameBoardState[row][column]) {
+        case EMPTY:
+          messageButton.setLabel(" ").setStyle("SECONDARY");
+          break;
+        case PLAYER1:
+          messageButton.setLabel("X").setStyle("PRIMARY");
+          break;
+        case PLAYER2:
+          messageButton.setLabel("O").setStyle("DANGER");
+          break;
+      }
       actionRow.addComponents(messageButton);
     }
     components.push(actionRow);
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,63 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
   name: "interactionCreate",
   execute(interaction) {
+    if (interaction.isButton()) {
+      //----Beginning of handling tictactoe interactions----//
+      if (interaction.customId.includes("tictactoe")) {
+        //Store all information from the clicked button
+        var clickedRowNumber = interaction.customId[10];
+        var clickedColNumber = interaction.customId.substring(
+          interaction.customId.length - 1
+        );
+        var clickedButton =
+          interaction.message.components[clickedRowNumber].components[
+            clickedColNumber
+          ];
+        var clickedRowComp = interaction.message.components[clickedRowNumber];
+
+        //current player
+        var labelLength = interaction.message.embeds[0].title.length;
+        var currentPlayer = interaction.message.embeds[0].title.substring(
+          labelLength - 1
+        );
+
+        //update based on which player's turn it is
+        clickedButton.label = currentPlayer;
+
+        //taking turns
+        if (currentPlayer == "X") {
+          currentPlayer = "O";
+        } else {
+          currentPlayer = "X";
+        }
+
+        //value of current player's turn
+        var newPlayer = "Turn: " + currentPlayer;
+
+        //create a new board and update the buttons
+        var components = [];
+
+        for (var i = 0; i < 3; i++) {
+          var component = interaction.message.components[i];
+          if (i != clickedRowNumber) {
+            components.push(component);
+          } else {
+            components.push(clickedRowComp);
+          }
+        }
+
+        //update interaction
+        interaction.update({
+          embeds: [new MessageEmbed().setTitle(newPlayer)],
+          components: [components[0], components[1], components[2]],
+        });
+      }
+      //----End of tictactoe interactions----//
+    }
+
+    //log when someone triggers an interaction in the specific channel
     console.log(
       `${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`
     );
